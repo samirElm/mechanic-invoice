b99e2b4f7f58a543171ad83c3500a0f7f5c18c86
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2015-07-10 20:14:23 +0200

    invoiced parts setup; validations ok

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,7 @@ class Invoice < ActiveRecord::Base
   belongs_to :customer
 
   has_many :invoiced_parts
+  has_many :parts, through: :invoiced_parts
+
+  accepts_nested_attributes_for :invoiced_parts
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,4 @@
 class Part < ActiveRecord::Base
   has_many :invoiced_parts
+  has_many :invoices, through: :invoiced_parts
 end
