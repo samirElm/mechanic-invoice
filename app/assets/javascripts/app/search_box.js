9b262e6df2be66944de14563a389ea4d216e71ce
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2015-07-11 13:42:44 +0200

    custom search box js with dataset attributes

--- app/assets/javascripts/app/search_box.js
+++ app/assets/javascripts/app/search_box.js
@@ -6,13 +6,19 @@ searchServices = function (allDiv) {
   if(searchTerm.length > 0) {
     var elements = document.getElementsByClassName('div-content');
     var searchResultsContent = document.getElementById('search-results-content');
+    var noResultsFound = document.getElementById('no-results-found');
+
 
     allElements.style.display = 'none';
     searchResults.style.display = 'block';
 
     $(searchResultsContent).html('');
     jQuery.each( elements, function( i, element ) {
-      if (element.dataset.name.indexOf(searchTerm.toLowerCase()) > -1) {
+      if (element.dataset.customerName.indexOf(searchTerm.toLowerCase()) > -1) {
+        $(element).clone().appendTo(searchResultsContent)
+      } else if (element.dataset.descriptionMo.indexOf(searchTerm.toLowerCase()) > -1) {
+        $(element).clone().appendTo(searchResultsContent)
+      } else if (element.dataset.immat.indexOf(searchTerm.replace(/-/g, '')) > -1) {
         $(element).clone().appendTo(searchResultsContent)
       }
     });
@@ -21,4 +27,3 @@ searchServices = function (allDiv) {
     searchResults.style.display = 'none';
   }
 }
-
