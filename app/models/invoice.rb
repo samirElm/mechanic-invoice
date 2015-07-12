54de33c7dc8e279860219d59b0a8e5f01589a5f4
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2015-07-12 10:40:56 +0200

    handle all prices for invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,6 +6,7 @@ class Invoice < ActiveRecord::Base
   has_many :parts, through: :invoiced_parts
 
   monetize :total_price_cents
+  monetize :price_mo_cents
 
   accepts_nested_attributes_for :invoiced_parts, :reject_if => :all_blank, :allow_destroy => true
 
@@ -14,5 +15,20 @@ class Invoice < ActiveRecord::Base
       invoiced_part.price_ttc = invoiced_part.price_ht * 1.2
       invoiced_part.save
     end
+  end
+
+  def define_price_mo(mechanic)
+    self.price_mo = self.time_mo * mechanic.price_per_hour
+  end
+
+  def define_total_price
+    total_price_parts = 0
+
+    self.invoiced_parts.each do |invoiced_part|
+      total_price_parts += invoiced_part.price_ttc
+    end
+
+    self.total_price = total_price_parts + self.price_mo
+    self.save
   end
 end
