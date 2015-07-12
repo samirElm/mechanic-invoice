115d9d74d1f3a9a9733c4b522dbaf1788beb6e24
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2015-07-12 14:30:55 +0200

    dashboard setup and method displaying data

--- config/application.rb
+++ config/application.rb
@@ -39,8 +39,8 @@ module MechanicInvoice
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :fr
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :fr
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
